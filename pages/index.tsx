fb057cb24911ca8d25ca2c96396f800ae7b99932
harsh johar <hpsj3112@gmail.com>
2022-11-16 22:47:03 +0530

    timeline added (dummy data)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import {NextPage} from "next";
+import Timeline from '../components/Timeline';
 
 const Home: NextPage = ()=> {
     return (
-        <div>
-            Hello
+        <div className='text-center'>
+            <p>-- Insert good looking poster here --</p>
+            <p>-- some about us stuff --</p>
+            <p>-- sponsors --</p>
+            <div className='bg-blue-100'>
+
+            <p>Timeline</p>
+            <Timeline />
+            </div>
         </div>
     )
 }
