3bc7381331644212441e3d24bc1b1f302fa17a32
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-11-19 00:23:51 +0530

    lets see

--- components/leaderboard/TopCard.tsx
+++ components/leaderboard/TopCard.tsx
@@ -2,6 +2,8 @@ import {Item} from "../../public/types";
 
 export const TopCard = ({item}: {item: Item}) => {
     return (
-        <img src={item.user.avatar_url}/>
+        <div>
+
+        </div>
     )
 }
