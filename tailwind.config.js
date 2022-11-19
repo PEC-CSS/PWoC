734a288e925d01f041dd532b1fdc1ddff6a063fd
Rahul5430 <rahul213himani@gmail.com>
2022-11-19 19:45:08 +0530

    ui changes for project cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      height: {
-				"10v": "10vh",
-				"20v": "20vh",
-				"30v": "30vh",
-				"40v": "40vh",
-				"50v": "50vh",
-				"60v": "60vh",
-				"70v": "70vh",
-				"80v": "80vh",
-				"90v": "90vh",
-				"100v": "100vh",
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			height: {
+				'10v': '10vh',
+				'20v': '20vh',
+				'30v': '30vh',
+				'40v': '40vh',
+				'50v': '50vh',
+				'60v': '60vh',
+				'70v': '70vh',
+				'80v': '80vh',
+				'90v': '90vh',
+				'100v': '100vh',
 			},
-    },
-  },
-  plugins: [require('tailwind-scrollbar-hide')],
-}
+		},
+	},
+	plugins: [require('tailwind-scrollbar-hide')],
+};
