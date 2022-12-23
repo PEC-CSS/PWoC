2f206fb58948149e091ef08139dea8f0b071dd46
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-12-23 21:16:20 +0530

    Leaderboard ui fixed

--- components/leaderboard/LeaderboardTable.tsx
+++ components/leaderboard/LeaderboardTable.tsx
@@ -8,7 +8,8 @@ export const LeaderboardTable = ({leaderboard}: {leaderboard: Item[]}) => {
         fontWeight: "bold",
         fontSize: "25px",
         color: "white",
-        backgroundColor: "black"
+        backgroundColor: "rgba(0,0,0,0.5)",
+        backdropFilter: "blur( 8px )",
     }
 
     return (
@@ -32,9 +33,13 @@ export const LeaderboardTable = ({leaderboard}: {leaderboard: Item[]}) => {
                 </TableRow>
             </TableHead>
             <TableBody sx={{
-                boxShadow: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
                 borderBottomLeftRadius: "20px",
-                borderBottomRightRadius: "20px"
+                borderBottomRightRadius: "20px",
+                backgroundColor: "rgba(73,73,73,0.7)",
+                backdropFilter: "blur( 8px )",
+                boxShadow: "0 8px 32px 0 rgba( 0, 0, 0, 0.18 );",
+                borderRadius: "10px",
+                border: "1px solid rgba( 255, 255, 255, 0.18 )",
             }}>
                 {
                     leaderboard.map((item: Item,i: number)=> {
