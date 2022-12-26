d30e843ac6d5cb1a425bdaf859943f42cb79f280
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-12-27 02:23:54 +0530

    Trying a fix

--- components/faqs/cards.tsx
+++ components/faqs/cards.tsx
@@ -4,9 +4,9 @@ export const Card = ({ faq }: { faq: Faq }) => {
 
 	return (
 		<div className='card glassmorphism bg-[rgba(55,65,81,0.3)] p-7 rounded-2xl h-auto'>
-			<div className='icon'>{faq.icon}</div>
-			<div className='title'>{faq.title}</div>
-			<div className='description'>{faq.description}</div>
+			<div className='faq-icon'>{faq.icon}</div>
+			<div className='faq-title'>{faq.title}</div>
+			<div className='faq-description'>{faq.description}</div>
 		</div>
 	);
 };
