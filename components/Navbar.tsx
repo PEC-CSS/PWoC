4a55bacac86047a0970c0dce5bba329665083f1f
Rahul5430 <rahul213himani@gmail.com>
2023-01-10 02:27:19 +0530

    testing for seo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,152 +1,161 @@
-import Link from "next/link";
-import { useState } from "react";
-import { AiOutlineMenuUnfold, AiOutlineMenuFold } from "react-icons/ai";
-import { TbHome2, TbLayoutDashboard } from "react-icons/tb";
-import { HiTemplate } from "react-icons/hi";
-import { GiSellCard } from "react-icons/gi";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import pwoc_text from "../public/assets/logo/pwoc_text.png"
-import {BiCalendarEvent} from "react-icons/bi";
+import Link from 'next/link';
+import { useState } from 'react';
+import { AiOutlineMenuUnfold, AiOutlineMenuFold } from 'react-icons/ai';
+import { TbHome2, TbLayoutDashboard } from 'react-icons/tb';
+import { HiTemplate } from 'react-icons/hi';
+import { GiSellCard } from 'react-icons/gi';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import pwoc_text from '../public/assets/logo/pwoc_text.png';
+import { BiCalendarEvent } from 'react-icons/bi';
 
 export const Navbar = () => {
-    const [isMobileNavOpen, setisMobileNavOpen] = useState(false); // For toggling the mobile nav
-    const router = useRouter();
+	const [isMobileNavOpen, setisMobileNavOpen] = useState(false); // For toggling the mobile nav
+	const router = useRouter();
 
-    const handleClick = () => {
-        setisMobileNavOpen(!isMobileNavOpen);
-    };
+	const handleClick = () => {
+		setisMobileNavOpen(!isMobileNavOpen);
+	};
 
-    return (
-        <div>
-            <div className="flex flex-wrap ">
-                <div className="w-full ">
-                    <div className="pb-0 py-2 px-2 mx-auto">
-                        <div className="glassmorphism bg-[#161a1dff] w-full flex justify-between items-center p-2 text-gray-900 rounded-lg shadow-lg font-medium capitalize">
-                            {/* Logo */}
-                            <Link href="/">
-                                <span className="px-2 mr-2 md:border-r border-gray-800">
-                                    <Image
-                                        src={pwoc_text}
-                                        alt="PWoC"
-                                        height={100}
-                                        width={400}
-                                        className="w-[9rem] h-12 -mt-1 inline mx-auto"
-                                    />
-                                </span>
-                            </Link>
-                            <div className="px-2 md:flex gap-x-4 [@media(max-width:840px)]:gap-x-[1px] items-center flex-1 text-gray-900 font-medium capitalize hidden justify-end">
-                                {/* Links */}
-                                {navLinks?.map(({ title, link, icon }, id) => (
-                                    <Link key={id} href={link}>
-                                        <p
-                                            //   id={id}
-                                            className={`px-2 py-1 flex items-center cursor-pointer text-white hover:bg-gray-100 hover:text-[#2d363c] text-sm rounded ${router.pathname == link
-                                                ? "underline underline-offset-4 decoration-2 text-[#2d363c] bg-gray-100"
-                                                : ""
-                                                }`}
-                                        >
-                                            <span className="p-2 bg-black rounded-full text-white">
-                                                {icon}
-                                            </span>
-                                            <span className="mx-1 text-semibold">
-                                                {title}
-                                            </span>
-                                        </p>
-                                    </Link>
-                                ))}
-                            </div>
-                            {/* Hamburger Menu  */}
-                            <div className="md:hidden transition-all mr-3 my-3 cursor-pointer text-white hover:text-gray-700">
-                                {isMobileNavOpen ? (
-                                    <AiOutlineMenuFold
-                                        onClick={() => handleClick()}
-                                        className="rounded text-2xl"
-                                    />
-                                ) : (
-                                    <AiOutlineMenuUnfold
-                                        onClick={() => handleClick()}
-                                        className="rounded text-2xl"
-                                    />
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                    {/* Mobile Navbar */}
-                    <div
-                        id="navbar"
-                        className={`pt-0 fixed z-[100] top-2 mx-auto ${isMobileNavOpen
-                            ? "translate-x-0"
-                            : "-translate-x-full"
-                            } transition-all flex-wrap md:hidden`}
-                    >
-                        <div className="py-[.5px] w-64">
-                            <div className="w-full py-4 space-y-6 px-2 text-gray-900 glassmorphism bg-[rgba(0,0,0,0.7)] rounded-lg min-h-screen text-left capitalize font-medium shadow-lg">
-                                {/* Logo */}
-                                <Link href="/">
-                                    <div className="px-2 grid place-items-center md:border-r border-gray-800">
-                                        <Image
-                                            src={pwoc_text}
-                                            alt="PWoC"
-                                            height={100}
-                                            width={300}
-                                            className="w-[9rem] h-12 -mt-1 inline mx-auto"
-                                        />
-                                    </div>
-                                </Link>
+	return (
+		<div>
+			<div className='flex flex-wrap '>
+				<div className='w-full '>
+					<div className='pb-0 py-2 px-2 mx-auto'>
+						<div className='glassmorphism bg-[#161a1dff] w-full flex justify-between items-center p-2 text-gray-900 rounded-lg shadow-lg font-medium capitalize'>
+							{/* Logo */}
+							<Link href='/'>
+								<span className='px-2 mr-2 md:border-r border-gray-800'>
+									<Image
+										src={pwoc_text}
+										alt='PWoC'
+										height={135}
+										width={435}
+										// height={100}
+										// width={400}
+										// className='w-[9rem] h-12 -mt-1 inline mx-auto'
+										className='w-[9rem] h-11 -mt-1 inline mx-auto'
+									/>
+								</span>
+							</Link>
+							<div className='px-2 md:flex gap-x-4 [@media(max-width:840px)]:gap-x-[1px] items-center flex-1 text-gray-900 font-medium capitalize hidden justify-end'>
+								{/* Links */}
+								{navLinks?.map(({ title, link, icon }, id) => (
+									<Link key={id} href={link}>
+										<p
+											//   id={id}
+											className={`px-2 py-1 flex items-center cursor-pointer text-white hover:bg-gray-100 hover:text-[#2d363c] text-sm rounded ${
+												router.pathname == link
+													? 'underline underline-offset-4 decoration-2 text-[#2d363c] bg-gray-100'
+													: ''
+											}`}
+										>
+											<span className='p-2 bg-black rounded-full text-white'>
+												{icon}
+											</span>
+											<span className='mx-1 text-semibold'>
+												{title}
+											</span>
+										</p>
+									</Link>
+								))}
+							</div>
+							{/* Hamburger Menu  */}
+							<div className='md:hidden transition-all mr-3 my-3 cursor-pointer text-white hover:text-gray-700'>
+								{isMobileNavOpen ? (
+									<AiOutlineMenuFold
+										onClick={() => handleClick()}
+										className='rounded text-2xl'
+									/>
+								) : (
+									<AiOutlineMenuUnfold
+										onClick={() => handleClick()}
+										className='rounded text-2xl'
+									/>
+								)}
+							</div>
+						</div>
+					</div>
+					{/* Mobile Navbar */}
+					<div
+						id='navbar'
+						className={`pt-0 fixed z-[100] top-2 mx-auto ${
+							isMobileNavOpen
+								? 'translate-x-0'
+								: '-translate-x-full'
+						} transition-all flex-wrap md:hidden`}
+					>
+						<div className='py-[.5px] w-64'>
+							<div className='w-full py-4 space-y-6 px-2 text-gray-900 glassmorphism bg-[rgba(0,0,0,0.7)] rounded-lg min-h-screen text-left capitalize font-medium shadow-lg'>
+								{/* Logo */}
+								<Link href='/'>
+									<div className='px-2 grid place-items-center md:border-r border-gray-800'>
+										<Image
+											src={pwoc_text}
+											alt='PWoC'
+											height={135}
+											width={435}
+											// height={100}
+											// width={300}
+											// className='w-[9rem] h-12 -mt-1 inline mx-auto'
+											className='w-[9rem] h-11 -mt-1 inline mx-auto'
+										/>
+									</div>
+								</Link>
 
-                                {/* Links */}
-                                {navLinks?.map(({ title, link, icon }, id) => (
-                                    <Link key={id} href={link}>
-                                        <p
-                                            //   id={id}
-                                            className={`p-2 my-2 flex items-center cursor-pointer text-white text-sm rounded z-100 ${router.pathname == link
-                                                ? "underline underline-offset-4 decoration-2 text-[#2d363c] bg-gray-100"
-                                                : ""
-                                                }`}
-                                        >
-                                            <span className="p-2 bg-black text-white rounded-full">
-                                                {icon}
-                                            </span>
-                                            <span className="mx-1 text-semibold">
-                                                {title}
-                                            </span>
-                                        </p>
-                                    </Link>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+								{/* Links */}
+								{navLinks?.map(({ title, link, icon }, id) => (
+									<Link key={id} href={link}>
+										<p
+											//   id={id}
+											className={`p-2 my-2 flex items-center cursor-pointer text-white text-sm rounded z-100 ${
+												router.pathname == link
+													? 'underline underline-offset-4 decoration-2 text-[#2d363c] bg-gray-100'
+													: ''
+											}`}
+										>
+											<span className='p-2 bg-black text-white rounded-full'>
+												{icon}
+											</span>
+											<span className='mx-1 text-semibold'>
+												{title}
+											</span>
+										</p>
+									</Link>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const navLinks = [
-    {
-        title: "Home",
-        link: "/",
-        icon: <TbHome2 />,
-    },
-    {
-        title: "Leaderboard",
-        link: "/leaderboard",
-        icon: <TbLayoutDashboard />,
-    },
-    {
-        title: "Projects",
-        link: "/projects",
-        icon: <HiTemplate />,
-    },
-    {
-        title: "Events",
-        link: "/events",
-        icon: <BiCalendarEvent />,
-    },
-    {
-        title: "FAQ's",
-        link: "/faq",
-        icon: <GiSellCard />,
-    },
+	{
+		title: 'Home',
+		link: '/',
+		icon: <TbHome2 />,
+	},
+	{
+		title: 'Leaderboard',
+		link: '/leaderboard',
+		icon: <TbLayoutDashboard />,
+	},
+	{
+		title: 'Projects',
+		link: '/projects',
+		icon: <HiTemplate />,
+	},
+	{
+		title: 'Events',
+		link: '/events',
+		icon: <BiCalendarEvent />,
+	},
+	{
+		title: "FAQ's",
+		link: '/faq',
+		icon: <GiSellCard />,
+	},
 ];
--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,100 +1,109 @@
-import { NextPage } from "next";
-import { Octokit } from "@octokit/rest";
-import { contributors, repositories } from "../public/data";
-import { useEffect, useState } from "react";
-import { Item, PullRequest } from "../public/types";
-import { LeaderboardTable } from "../components/leaderboard/LeaderboardTable";
-import Head from "next/head";
-import Lottie from "react-lottie-player";
-import snowman from '../public/assets/animations/snowman.json'
-import { TopThree } from "../components/leaderboard/TopThree";
-import PageLayout from "../components/layout/PageLayout";
+import { NextPage } from 'next';
+import { Octokit } from '@octokit/rest';
+import { contributors, repositories } from '../public/data';
+import { useEffect, useState } from 'react';
+import { Item, PullRequest } from '../public/types';
+import { LeaderboardTable } from '../components/leaderboard/LeaderboardTable';
+import Head from 'next/head';
+import Lottie from 'react-lottie-player';
+import snowman from '../public/assets/animations/snowman.json';
+import { TopThree } from '../components/leaderboard/TopThree';
+import PageLayout from '../components/layout/PageLayout';
 
 const Leaderboard: NextPage = () => {
+	const [leaderboard, setLeaderboard] = useState<Item[]>([]);
 
-    const [leaderboard, setLeaderboard] = useState<Item[]>([])
+	const getPullRequests = async () => {
+		let octokit = new Octokit({ auth: process.env.ACCESS_TOKEN });
+		let repoRequests = repositories.map((repo: string) => {
+			let query = `type:pr+repo:${repo}`;
+			contributors.forEach((user) => {
+				query += `+author:${user.username}`;
+			});
+			return octokit.rest.search
+				.issuesAndPullRequests({
+					q: query,
+					per_page: 100,
+				})
+				.then((res) => res.data.items);
+		});
+		let repoResponses = (await Promise.all(
+			repoRequests
+		)) as PullRequest[][];
+		let pullRequestMap = new Map<string, PullRequest[]>();
+		let nameMap = new Map<string, string>();
+		contributors.forEach((user) => {
+			pullRequestMap.set(user.username, []);
+			nameMap.set(user.username, user.name);
+		});
+		repoResponses.forEach((pullRequests: PullRequest[]) => {
+			pullRequests.forEach((pullRequest: PullRequest) => {
+				pullRequestMap.get(pullRequest.user.login)?.push(pullRequest);
+			});
+		});
+		let leaderboard: Item[] = [];
+		pullRequestMap.forEach(
+			(pullRequests: PullRequest[], username: string) => {
+				if (pullRequests.length === 0) return;
+				leaderboard.push({
+					user: {
+						username: username,
+						name: nameMap.get(username) || username,
+						avatar_url: pullRequests[0].user.avatar_url,
+						html_url: pullRequests[0].user.html_url,
+					},
+					pullRequests: pullRequests,
+					points: pullRequests.length,
+				});
+			}
+		);
+		leaderboard.sort((item1: Item, item2: Item) => {
+			return item2.points - item1.points;
+		});
+		console.log(leaderboard);
+		return leaderboard;
+	};
 
-    const getPullRequests = async () => {
-        let octokit = new Octokit({ auth: process.env.ACCESS_TOKEN })
-        let repoRequests = repositories.map((repo: string) => {
-            let query = `type:pr+repo:${repo}`
-            contributors.forEach((user) => {
-                query += `+author:${user.username}`
-            })
-            return octokit.rest.search.issuesAndPullRequests({
-                q: query,
-                per_page: 100
-            }).then(res => res.data.items)
-        })
-        let repoResponses = await Promise.all(repoRequests) as PullRequest[][]
-        let pullRequestMap = new Map<string, PullRequest[]>()
-        let nameMap = new Map<string, string>()
-        contributors.forEach((user) => {
-            pullRequestMap.set(user.username, [])
-            nameMap.set(user.username, user.name)
-        })
-        repoResponses.forEach((pullRequests: PullRequest[]) => {
-            pullRequests.forEach((pullRequest: PullRequest) => {
-                pullRequestMap.get(pullRequest.user.login)?.push(pullRequest)
-            })
-        })
-        let leaderboard: Item[] = []
-        pullRequestMap.forEach((pullRequests: PullRequest[], username: string) => {
-            if (pullRequests.length === 0)
-                return
-            leaderboard.push({
-                user: {
-                    username: username,
-                    name: nameMap.get(username) || username,
-                    avatar_url: pullRequests[0].user.avatar_url,
-                    html_url: pullRequests[0].user.html_url
-                },
-                pullRequests: pullRequests,
-                points: pullRequests.length
-            })
-        })
-        leaderboard.sort((item1: Item, item2: Item) => {
-            return item2.points - item1.points
-        })
-        console.log(leaderboard)
-        return leaderboard
-    }
+	useEffect(() => {
+		getPullRequests()
+			.then((itemList) => setLeaderboard(itemList))
+			.catch((error) => console.error(error));
+	}, []);
 
-    useEffect(() => {
-        getPullRequests()
-            .then(itemList => setLeaderboard(itemList))
-            .catch(error => console.error(error))
-    }, [])
-
-    return (
-
-        <PageLayout title="PWOC | Leaderboard">
-            <div className="flex items-center flex-col">
-                <Head>
-                    <title>Leaderboard</title>
-                    <meta name="description" content="Leaderboard for winter of code" />
-                </Head>
-                {
-                    leaderboard.length > 0 && process.env.NEXT_PUBLIC_DEV ? (
-                        <>
-                            <TopThree topList={leaderboard.slice(0, 3)} />
-                            <LeaderboardTable leaderboard={leaderboard} />
-                        </>
-                    ) : (
-                        <div className="flex flex-col items-center my-[30px]">
-                            <Lottie
-                                play
-                                loop
-                                animationData={snowman}
-                                className="h-[300px] w-auto my-[30px]"
-                            />
-                            <div className="font-bold text-[30px] animate-pulse">Preparing the leaderboard...</div>
-                        </div>
-                    )
-                }
-            </div>
-        </PageLayout>
-    )
-}
+	return (
+		<PageLayout
+			title='PWOC | Leaderboard'
+			description='Leaderboard of PWoC, based on PR count in the given period of the event. Only participants with merged PRs appear here.'
+		>
+			<div className='flex items-center flex-col'>
+				{/* <Head>
+					<title>Leaderboard</title>
+					<meta
+						name='description'
+						content='Leaderboard for winter of code'
+					/>
+				</Head> */}
+				{leaderboard.length > 0 && process.env.NEXT_PUBLIC_DEV ? (
+					<>
+						<TopThree topList={leaderboard.slice(0, 3)} />
+						<LeaderboardTable leaderboard={leaderboard} />
+					</>
+				) : (
+					<div className='flex flex-col items-center my-[30px]'>
+						<Lottie
+							play
+							loop
+							animationData={snowman}
+							className='h-[300px] w-auto my-[30px]'
+						/>
+						<div className='font-bold text-[30px] animate-pulse'>
+							Preparing the leaderboard...
+						</div>
+					</div>
+				)}
+			</div>
+		</PageLayout>
+	);
+};
 
 export default Leaderboard;
